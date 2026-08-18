90c61fd233b6780eff68dc3f6927fed3434404d8
Alonso Mora <113932813+AlsoMora@users.noreply.github.com>
2026-08-17 21:36:32 -0600

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const objAux = {
+            "[":"]",
+            "{":"}",
+            "(":")"
+        }
+        
+        let stack = []
+
+        for(let c of s){
+            if(stack.lengt === 0) {
+                stack.push(c)
+                continue
+            }
+
+            if (objAux[stack[stack.length-1]] === c){
+                stack.pop()
+            }else{
+                stack.push(c)
+            }
+        }
+
+        return stack.length === 0
+    }
+}
